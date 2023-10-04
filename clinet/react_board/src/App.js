8a0644072433b47cd28d3ec21b1018170aae3ac1
lunadein2022 <lunadein2022@gmail.com>
2023-10-04 22:11:43 +0900

    client, app.js 수정

--- clinet/react_board/src/App.js
+++ clinet/react_board/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BoardList from './Board/BoardList';
+import Write from './Board/Write';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // React Router를 불러옴
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          isModifyMode: false,
+          isComplete: true,
+          boardId: 0,
+      };
+  }
+
+  handleModify = (checkList) => {
+      if (checkList.length === 0) {
+          alert("수정할 게시글을 선택하세요.");
+      } else if (checkList.length > 1) {
+          alert("하나의 게시글만 선택하세요.");
+      }
+
+      this.setState({
+          isModifyMode: checkList.length === 1,
+          boardId: checkList[0] || 0,
+      });
+  };
+
+  handleCancel = () => {
+      this.setState({
+          isModifyMode: false,
+          isComplete: false,
+          boardId: 0,
+      });
+  };
+
+  renderComplete = () => {
+      this.setState({
+          isComplete: true,
+      });
+  };
+
+  render() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<BoardList isModifyMode={this.state.isModifyMode} boardId={this.state.boardId} />} />
+                    <Route
+                        path="/write"
+                        element={<Write
+                            isModifyMode={this.state.isModifyMode}
+                            boardId={this.state.boardId}
+                            handleCancel={this.handleCancel}
+                        />}
+                    />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
+
+}
+
 
 export default App;
